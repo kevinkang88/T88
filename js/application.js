dc401fc19c754c9e9c6299dab0739e9567e157e2
Kevin Kang <kevinkang88@gmail.com>
2014-08-04 16:38:13 -0700

    tested that jquery libary is loaded and application js created

---
+++ js/application.js
@@ -0,0 +1,12 @@
+// everything will compile to this file
+
+
+if (typeof jQuery != 'undefined') {
+
+    alert("jQuery library is loaded!");
+
+}else{
+
+    alert("jQuery library is not found!");
+
+}
